d07ce172c3c2612be3bdb94ca319fd51f2243763
alexgheorghe1 <Alex.gheorghe@kellogg.northwestern.edu>
2022-02-22 17:30:05 +0000

    class exercises

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,17 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.where({user_id: current_user.id}
+    )
     @task = Task.new
   end
 
   def create
     @task = Task.new(params["task"])
+    @task.user_id = @current_user.id
     @task.save
     redirect_to "/tasks"
   end
+
 
   def destroy
     @task = Task.find(params["id"])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    # Parameters: "user"=>
+    # {"first_name"=>"Alex", 
+    #"last_name"=>"Gheorghe"
+    # "email"=>"akkk"
+
+    plain_text_password = params["user"]["password"] # = akkk
     @user = User.new(params["user"])
+    @user.password = BCrypt::Password.create(plain_text_password)
     @user.save
     redirect_to "/users/#{@user.id}"
   end
